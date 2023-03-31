89118b3da4ebc86b72a956b762919df6af943140
Keyur Bisetty <keyur@Keyurs-MacBook-Pro.local>
2023-03-31 13:34:14 -0400

    install dependencies and updated app.js with them

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Parse incoming JSON data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Connect to MongoDB database using the connection URI in the environment variable
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+// Start the server and listen on the specified port
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
